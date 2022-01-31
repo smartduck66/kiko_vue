eecb62ab8c23b032e9cd29a044aec2e3ef3ac6de
André Tonic <andre_tonic@yahoo.fr>
2022-01-31 10:11:56 +0100

    index.html

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-# Vue 3 + Typescript + Vite
+# Cette nouvelle version de kiko s'appuie sur Vue 3 + Typescript + Vite
 
-This template should help get you started developing with Vue 3 and Typescript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Derniers travaux : 31/1/2022 - Initialisation du projet avec Vue3
 
-## Recommended IDE Setup
+-------------------------------------------------------------------------------------------------------
+Nouvelles extensions IDE VSCODE installées : Vetur et Volar
 
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+Pour développer et tester en local :
+. npm run dev
+. http://localhost:3000 lance le vite dev server (on bénéficie immédiatement du HMR, Hot Module Reload)
 
-## Type Support For `.vue` Imports in TS
-
-Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's `.vue` type support plugin by running `Volar: Switch TS Plugin on/off` from VSCode command palette.
+Pour construire le site statique (->dist) qui sera publié sur netlify :
+. npm run build
+. npm run preview
+. http://localhost:5000 pour tester en local
+. git add -A
+. git commit -m "new fonctions"
+. git push
+. Netlify le publie automatiquement en production sur https://musing-ptolemy-db8901.netlify.app/
+-------------------------------------------------------------------------------------------------------
---
+++ src/csv_to_json.ts
@@ -0,0 +1,84 @@
+// Utilitaire "mode batch" (node csv_to_json.js) permettant de créer des fichiers json à partir de fichiers csv 'open data'
+// ************************************************************************************************************************
+
+// Transformation d'un fichier CSV des communes françaises (https://www.data.gouv.fr/fr/datasets/base-officielle-des-codes-postaux/)
+// en un fichier json contenant le nom, la latitude et la longitude de chaque commune française référencée
+// Import de communes.json dans fauna via shell : fauna import --path=./assets/data/communes.json --collection=communes --append
+class communes {
+  cp: string;
+  ville: string;
+  latitude: number;
+  longitude: number;
+
+  constructor() {
+    this.cp = "";
+    this.ville = "";
+    this.latitude = 0;
+    this.longitude = 0;
+  }
+}
+
+const fiches: communes[] = [];
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import * as fs1 from "fs";
+
+// Balayage du fichier csv, enrichissement de l'Array fiches, création du JSON sur disque
+// Format : Code_commune_INSEE;Nom_commune;Code_postal;Ligne_5;Libellé_d_acheminement;coordonnees_gps
+// Ex : 05024;VALDOULE;05150;STE MARIE;VALDOULE;44.4677366709,5.50388863719
+
+const text = fs1.readFileSync("../data_source/communes.csv", "utf8");
+const allTextLines = text.split(/\r\n|\n/);
+
+for (let i = 0; i < allTextLines.length; i++) {
+  const item = new communes(); // note the "new" keyword here
+
+  const fields = allTextLines[i].split(";");
+
+  item.cp = fields[2];
+  item.ville = fields[1];
+  const coords = fields[5].split(",");
+  item.latitude = Number(coords[0]);
+  item.longitude = Number(coords[1]);
+
+  fiches.push(item); // Enrichissement du 'vecteur' contenant l'ensemble des fiches
+}
+
+fs1.writeFileSync("../data/communes.json", JSON.stringify(fiches, null, 2)); // Création du json final sur disque
+
+// ***********************************************************************************************************************************
+
+// Transformation d'un fichier CSV des sites Seveso (https://public.opendatasoft.com/explore/dataset/sites-seveso/export/?flg=fr&location=9,44.52588,1.0643&basemap=jawg.streets)
+// en un fichier json contenant le nom de l'usine et la commune hébergeant chaque site classé seveso, la latitude, la longitude
+
+class seveso {
+  site: string;
+  latitude: number;
+  longitude: number;
+
+  constructor() {
+    this.site = "";
+    this.latitude = 0;
+    this.longitude = 0;
+  }
+}
+
+const fiches1: seveso[] = [];
+
+// Balayage du fichier csv, enrichissement de l'Array fiches, création du JSON sur disque
+const text1 = fs1.readFileSync("../data_source/sites-seveso.csv", "utf8");
+const allTextLines1 = text1.split(/\r\n|\n/);
+
+for (let i1 = 0; i1 < allTextLines1.length; i1++) {
+  const item1 = new seveso(); // note the "new" keyword here
+
+  const fields1 = allTextLines1[i1].split(";");
+
+  const coords1 = fields1[0].split(",");
+  item1.site = "Sté " + fields1[2] + " à " + fields1[3] + " - " + fields1[12];
+  item1.latitude = Number(coords1[0]);
+  item1.longitude = Number(coords1[1]);
+
+  fiches1.push(item1); // Enrichissement du 'vecteur' contenant l'ensemble des fiches
+}
+
+fs1.writeFileSync("../data/seveso.json", JSON.stringify(fiches1, null, 2)); // Création du json final sur disque
