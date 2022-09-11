b3335e109f5f8b6d7ef3fb91c7b32324f88da7d1
André Tonic <andre_tonic@yahoo.fr>
2022-09-11 15:56:14 +0200

    v2.00a

--- src/components/Station78.vue
+++ src/components/Station78.vue
@@ -1,46 +1,89 @@
 <script setup lang="ts">
+import Panel from "primevue/panel";
 defineProps<{ valeur_ref: string[] }>();
 </script>
 
 <template>
-  <h4>{{ valeur_ref[0] }}</h4>
-
-  <p>
-    <span class="icon-text">
-      <span class="icon">
-        <i class="fas fa-thermometer-half"></i>
-      </span>
-      <span id="tmoy"></span>
-      {{ valeur_ref[1] }}
-    </span>
-  </p>
-  <p>
-    <span class="icon-text">
-      <span class="icon">
-        <i class="fas fa-temperature-low"></i>
-      </span>
-      <span id="tmin"></span>
-      {{ valeur_ref[2] }}
-    </span>
-  </p>
-  <p>
-    <span class="icon-text">
-      <span class="icon">
-        <i class="fas fa-temperature-high"></i>
-      </span>
-      <span id="tmax"></span>
-      {{ valeur_ref[3] }}
-    </span>
-  </p>
-  <p>
-    <span class="icon-text">
-      <span class="icon">
-        <i class="fas fa-atom"></i>
-      </span>
-      <span id="cnpe"></span>
-      {{ valeur_ref[4] }}
-    </span>
-  </p>
+  <Panel v-bind="{ header: valeur_ref[0] }" :toggleable="true">
+    <div class="my_grid">
+      <div class="c-item-1">
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-thermometer-half"></i>
+            </span>
+            <span id="tmoy"></span>
+            {{ valeur_ref[1] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-temperature-low"></i>
+            </span>
+            <span id="tmin"></span>
+            {{ valeur_ref[2] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-temperature-high"></i>
+            </span>
+            <span id="tmax"></span>
+            {{ valeur_ref[3] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-atom"></i>
+            </span>
+            <span id="cnpe"></span>
+            {{ valeur_ref[4] }}
+          </span>
+        </p>
+      </div>
+      <div class="c-item-2">
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-sun"></i>
+            </span>
+            <span id="soleil"></span>
+            {{ valeur_ref[5] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-cloud-rain"></i>
+            </span>
+            <span id="pluie"></span>
+            {{ valeur_ref[6] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-wind"></i>
+            </span>
+            <span id="vent"></span>
+            {{ valeur_ref[7] }}
+          </span>
+        </p>
+        <p>
+          <span class="icon-text">
+            <span class="icon">
+              <i class="fas fa-home"></i>
+            </span>
+            <span id="prix"></span>
+            {{ valeur_ref[8] }}
+          </span>
+        </p>
+      </div>
+    </div>
+  </Panel>
 </template>
 
 <style scoped>
@@ -58,5 +101,23 @@ code {
   padding: 2px 4px;
   border-radius: 4px;
   color: #304455;
+}
+
+.my_grid {
+  display: grid;
+  grid-template-columns: auto auto;
+}
+
+[class^="c-item"] {
+  display: inline-grid;
+}
+
+.c-item-1 {
+  grid-column: 1;
+  justify-content: left;
+}
+.c-item-2 {
+  grid-column: 2;
+  justify-content: left;
 }
 </style>
