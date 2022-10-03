ae62e1873e1180801a74128eaa9bd4b264758a2d
André Tonic <andre_tonic@yahoo.fr>
2022-10-03 16:44:05 +0200

    fp1

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -208,7 +208,7 @@ async function onFastSearchCommune_serverless1(criteres: any) {
 
   const API_URL = "/.netlify/functions/database?code_postal=" + cp;
 
-  open.value = await fetch(API_URL)
+  await fetch(API_URL)
     .then(function (response) {
       return response.json();
     })
@@ -225,14 +225,13 @@ async function onFastSearchCommune_serverless1(criteres: any) {
       danger_cnpe.value = cnpe.site + " (" + Math.trunc(cnpe.distance) + "  kms)";
       danger_seveso.value = seveso.site + " (" + Math.trunc(seveso.distance) + "  kms)";
 
-      return true; // Affichage de la modale
+      open.value = true; // Affichage de la modale
     })
     .catch(function (err) {
       alert(
         "Le code saisi n'existe pas dans la base de référence des communes ou une erreur technique est survenue ! Veuillez saisir un autre code postal valide."
       );
       commune.value = 78190; // Réaffichage du code postal de référence
-      return false;
     });
 }
 
