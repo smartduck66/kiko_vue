778ed2edeb7bc22f1f92f8dae2a2c5f123555daa
André Tonic <andre_tonic@yahoo.fr>
2023-05-22 08:33:12 +0200

    new function

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -147,7 +147,7 @@ async function onFastSearchCommune_serverless(criteres: any) {
     );
     vd_commune.value = 78190; // Réaffichage du code postal de référence
   } else {
-    alert(response)
+    alert(response.data)
     const result = (await response.json()).data;
     const ville: string = result[0].ville;
     const lat: number = result[0].latitude;
