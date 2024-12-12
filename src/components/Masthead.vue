2a39711c9cf884b6e5b0dd90e8e01cfc91a0e167
André Tonic <andre_tonic@yahoo.fr>
2024-12-12 14:24:23 +0100

    new function

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -39,7 +39,7 @@ function RAZ_occurences() {
           <span class="sous-titre">Données climatiques France & DOM-TOM (1991-2020), mises à jour le {{ props.date_maj }}</span>
         </div>
         <div class="FlexWrapper_choix">
-          <Select class="menu" v-model="selectedOption" :options="optionsMenu" optionLabel="name" placeholder="v2.2" @update:modelValue="menu" />
+          <Select class="menu" v-model="selectedOption" :options="optionsMenu" optionLabel="name" placeholder="v2.2a" @update:modelValue="menu" />
           <Checkbox class="menu2" v-model="store.drias_checked" inputId="drias" :binary="true" @update:modelValue="RAZ_occurences" />
           <label for="drias" class="label"> Horizon 2050 </label>
         </div>
